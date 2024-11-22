3306a5aac0fd426a77be394f439bf4129f74a66e
Watts <1543099445121005@mil>
2024-11-22 11:35:22 -0600

    Mass changes made. Script made smaller, directories organized

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AutoSTIG
 Uses LGPO and GPO's to import STIG changes. PS1 script handles services and registry keys.
+
+Requires an updated version of the DISA Group Policies be added in the Sources\Policies directory.
+
+Requires admin privileges to use. Can run from anywhere.
--- STIG.bat
+++ STIG.bat
@@ -1,31 +1,15 @@
-:: Run this script to copy over the adml and admx files for Group Policy,
-:: Apply Local Group Policy, Fix services, and install Root Certs
-:: Run as Administrator to make it work
-                                                                        																													
-@ECHO off																																							
+@echo off
 pushd %~dp0
+setlocal
 
+echo Running STIG automation...
+call Bin\CopyTemplates.bat
+call Bin\ImportLocalPolicies.bat
+call Bin\ImportGroupPolicies.bat
+call Bin\ConfigureSystem.bat
 
-:: Places the .adml and .admx template files from stored directory to C:\Windows\PolicyDefinitions.
-::@echo.
-@copy "Windows OS Group Policy Templates\en-US\*.adml" "c:\Windows\PolicyDefinitions\en-US"
-@copy "Windows OS Group Policy Templates\*.admx" "c:\Windows\PolicyDefinitions"
-::@echo.
-
-
-:: Copies STIG'ed GPO's to the appropriate directory.
-@LGPO.exe /g "GPO\Windows 10"
-@LGPO.exe /s "GPO\Windows 10\DomainSysvol\GPO\Machine\microsoft\windows nt\SecEdit\GptTmpl.inf"
-@auditpol /restore /file:AuditPolicy
-@gpupdate /force
-
-
-:: Windows PowerShell Scripts
-@Powershell -ExecutionPolicy remotesigned -Command "& %CD%\Win10.ps1"
-
-@powercfg -SETACTIVE e9a42b02-d5df-448d-aa00-03f14749eb61
-
-:: Reboots the machine to apply all changes.
-@shutdown /r /t 15
+echo Rebooting to apply changes...
+shutdown /r /t 15
+endlocal
 popd
 exit
